fece34c4e153294ae7d95293b3177a19e26c6b1d
Roylan Pais <46266139+roylanpais@users.noreply.github.com>
2025-11-16 18:47:55 +0530

    Add Terraform required providers configuration

--- src/stack1/main.tf
+++ src/stack1/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "<6"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"  # Change this to your desired region
 }
